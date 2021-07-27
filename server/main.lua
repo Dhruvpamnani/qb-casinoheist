83d6f1957b84908cd5f759a91b0d25963be22815
AJ <66404074+ihyajb@users.noreply.github.com>
2021-07-27 10:44:00 -0400

    started cart loot

--- server/main.lua
+++ server/main.lua
@@ -22,9 +22,9 @@ RegisterServerEvent('qb-casinoheist:server:recieveCartItem')
 AddEventHandler('qb-casinoheist:server:recieveCartItem', function(type)
     local src = source
     local ply = QBCore.Functions.GetPlayer(src)
-    local info = {
-        worth = math.random(4000, 6000)
-    }
-    ply.Functions.AddItem('markedbills', math.random(1,4), false, info)
+   -- local info = {
+   --     worth = math.random(4000, 6000)
+   -- }
+   -- ply.Functions.AddItem('markedbills', math.random(1,4), false, info)
     TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items['markedbills'], "add")
 end)
